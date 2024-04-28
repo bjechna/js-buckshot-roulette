3b911478c129fc088863d77aba136e309e7c0799
Błażej Jechna <blazejjechna7@gmail.com>
2024-04-28 22:10:57 +0200

    Made game class

---
+++ classes/game.js
@@ -0,0 +1,45 @@
+class Game {
+    constructor(shots, max_hp, items) {
+        this.turn = "p";
+        this.items_get = 0;
+        this.shots = shots;
+        this.max_hp = max_hp;
+        this.items = items;
+    }
+
+    start_game() {
+
+    }
+
+    draw_shots() {
+
+    }
+
+    show_shots() {
+
+    }
+
+    suffle_shots(shots) {
+
+    }
+
+    draw_items(items_number, items) {
+
+    }
+
+    start_round() {
+
+    }
+
+    end_game() {
+
+    }
+
+    end_round() {
+
+    }
+
+    change_turn(turn) {
+
+    }
+}
