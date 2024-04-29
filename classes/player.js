8fe34cc1b728d947fb72f4a064fab7e50e06b4d3
Błażej Jechna <blazejjechna7@gmail.com>
2024-04-29 09:20:23 +0200

    Made methods in player class

--- classes/player.js
+++ classes/player.js
@@ -6,4 +6,24 @@ class Player {
         this.slots = slots;
         this.is_handcuffed = false;
     }
+
+    use_item(item) {
+        item.use(this);
+    }
+
+    add_item(item) {
+        this.items.push(item);
+    }
+
+    show_drawed_items() {
+        this.items.forEach((item) =>  {
+            if (!item[1]) {
+                item.show();
+            }
+        })
+    }
+
+    place_item(item, slot) {
+
+    }
 }
